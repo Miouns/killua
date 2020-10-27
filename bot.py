e1eaa1cda19a8c83e5d13f786233e10d588cc23a
Fabian maulana <54355288+Miouns@users.noreply.github.com>
2020-10-27 21:40:51 +0700

    update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
 import discord
+from discord.ext import commands
+import os
+
+
+bot = commands.AutoShardedBot(command_prefix="k!")
+
+@bot.event
+async def on_message(msg):
+  if msg.content == "k!hello":
+    mes = "Hello, {0.author.mention}".format(msg)
+    await bot.send_message(msg.channel, mes)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(os.environ["SECRET"])
