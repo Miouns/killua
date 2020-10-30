3a7cd883d5260bdce2e0e454d654a0e3312868db
Fabian maulana <54355288+Miouns@users.noreply.github.com>
2020-10-30 15:50:19 +0700

    update fun.py

---
+++ cogs/fun.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class General(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @commands.command(name="ping")
+  async def ping(self, ctx):
+    await ctx.send(f'Pong! {round(self.bot.latency*1000)}ms')
+  
+
+
+def setup(bot):
+  bot.add_cog(General(bot))
