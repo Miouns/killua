f6704dd22f4900e62c98fa384f995fc11d4a8e92
Fabian maulana <54355288+Miouns@users.noreply.github.com>
2020-10-30 16:05:13 +0700

    update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
 
+class General(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @commands.command(name="clear", aliases=["cl", 'purge'])
+  async def clear(self, ctx, *, amount):
+    if not amount:
+      return await ctx.send("Give me amount")
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f"Cleared {amount} Message(s)")
+
+def setup(bot):
+  bot.add_cog(General(bot))
