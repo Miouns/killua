c2fa7bc15b457946a49ce013692e966eb381a19d
Fabian maulana <54355288+Miouns@users.noreply.github.com>
2020-10-31 20:44:46 +0700

    update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from asyncio import sleep
+import json
 
 class Admin(commands.Cog):
   def __init__(self, bot):
@@ -15,6 +16,16 @@ class Admin(commands.Cog):
     msg = await ctx.send(f"<a:tick:769242226349572136> Cleared {amount} Messages")
     await sleep(5.0)
     await msg.delete()
+  
+  @commands.command(name="prefix", aliases=["ch", "changeprefix"])
+  async def prefix(self, ctx, *, prefix):
+  @commands.has_permissions(administrator=True)
+    with open("../config/prefix.json", "r+") as f:
+      data = {}
+      data[str(ctx.guild.id)] = prefix
+      json.dump(data, f)
+      f.close()
+    await ctx.send("Prefix set to `%s`" %(Prefix))
 
 def setup(bot):
   bot.add_cog(Admin(bot))
